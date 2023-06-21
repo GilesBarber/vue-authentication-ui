a25def009beafc4c2b3556dad3dd5afdaa66134b
GilesBarber <34550461+GilesBarber@users.noreply.github.com>
2023-06-21 12:28:51 +0100

    edit public path

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
 })
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/authentication/'
+    ? '/vue-authentication-ui/'
     : '/',
 
   transpileDependencies: [
